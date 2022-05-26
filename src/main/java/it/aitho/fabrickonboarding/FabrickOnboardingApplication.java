75f270361d7a31159193f6f2a6ead4454c314e5f
ggrillo <gabriele.grillo@aitho.it>
2022-05-26 16:28:24 +0200

    feature: Spring boot initializer

---
+++ src/main/java/it/aitho/fabrickonboarding/FabrickOnboardingApplication.java
@@ -0,0 +1,13 @@
+package it.aitho.fabrickonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aitho/fabrickonboarding/FabrickOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aitho.fabrickonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
