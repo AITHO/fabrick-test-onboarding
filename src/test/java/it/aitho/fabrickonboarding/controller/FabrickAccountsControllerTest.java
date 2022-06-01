00d3332846e7dbeceb8bbb4e814b010d477acfe3
ggrillo <gabriele.grillo@aitho.it>
2022-06-01 11:19:35 +0200

    feature: add controller test

--- src/test/java/it/aitho/fabrickonboarding/controller/FabrickAccountsControllerTest.java
+++ src/test/java/it/aitho/fabrickonboarding/controller/FabrickAccountsControllerTest.java
@@ -1,24 +1,33 @@
 package it.aitho.fabrickonboarding.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import it.aitho.fabrickonboarding.dto.accountbalance.AccountBalancePayload;
 import it.aitho.fabrickonboarding.service.AccountService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = FabrickAccountsController.class)
-class FabrickAccountsControllerTest {/* TODO
+class FabrickAccountsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-*/
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    public void shouldGetTheBalance() throws Exception {
+        var response = new AccountBalancePayload();
+        response.setBalance(15.0);
+        Mockito.when(accountService.retrieveAccountBalance("123456")).thenReturn(response);
+        this.mockMvc.perform(get("/accounts/123456")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value("15.0"));
+    }
 }
