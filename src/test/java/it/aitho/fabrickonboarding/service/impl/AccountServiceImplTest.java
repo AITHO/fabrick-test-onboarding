3ec6cc0a3a1b66766f2c45220f98ab5eda12e063
ggrillo <gabriele.grillo@aitho.it>
2022-06-01 11:24:28 +0200

    feature: retrieve transaction test

--- src/test/java/it/aitho/fabrickonboarding/service/impl/AccountServiceImplTest.java
+++ src/test/java/it/aitho/fabrickonboarding/service/impl/AccountServiceImplTest.java
@@ -3,6 +3,8 @@ package it.aitho.fabrickonboarding.service.impl;
 import it.aitho.fabrickonboarding.client.FabrickClient;
 import it.aitho.fabrickonboarding.dto.accountbalance.AccountBalanceDto;
 import it.aitho.fabrickonboarding.dto.accountbalance.AccountBalancePayload;
+import it.aitho.fabrickonboarding.dto.transactions.GetTransactionsResponseDto;
+import it.aitho.fabrickonboarding.dto.transactions.Transaction;
 import it.aitho.fabrickonboarding.enums.FabrickResponseStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 
@@ -39,5 +43,22 @@ class AccountServiceImplTest {
         var result = accountService.retrieveAccountBalance("accountId");
         Assertions.assertNotNull(result);
         Assertions.assertEquals(15.0, result.getBalance());
+    }
+
+    @Test
+    void retrieveTransactionsTest() {
+        var getTransactionsResponseDto = new GetTransactionsResponseDto();
+        var transaction = new Transaction();
+        transaction.setTransactionId("12345");
+        List<Transaction> list = List.of(transaction);
+        getTransactionsResponseDto.setList(list);
+
+        Mockito.when(fabrickClient.retrieveAccountTransactions(anyString(), anyString(), anyString())).thenReturn(getTransactionsResponseDto);
+
+        var result = accountService.retrieveAccountTransactions("accountId", "from", "to");
+        Assertions.assertNotNull(result);
+        Assertions.assertNotNull(result.getList());
+        Assertions.assertEquals(1, result.getList().size());
+        Assertions.assertEquals("12345", result.getList().get(0).getTransactionId());
     }
 }
